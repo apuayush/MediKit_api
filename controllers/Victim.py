8b56a2e4ff2c14b28aaa4abe77e124ce5ed1d62c
apurva <apuayush@gmail.com>
2019-03-25 15:16:54 +0530

    Victim info done

--- controllers/Victim.py
+++ controllers/Victim.py
@@ -12,13 +12,34 @@ class VictimInfo(RequestHandler):
         data = json.loads(self.request.body.decode('utf-8'))
         now = int(time.time())
         # post on dashboard
-        db.collection('session').document(str(time) + data['name']).set(data)
+        db.collection('session').document(str(now) + data['name']).set(data)
         # TODO - get similar description
         with open("controllers/secrets/descriptions.json") as f:
             desc_list = json.load(f)
             scored_list = process.extract(data['description'], desc_list, scorer=fuzz.token_sort_ratio,limit=10)
-            # TODO - get similar cases
-            print(scored_list)
-        self.write(json.dumps({"message": "wassup"}))
+        # TODO - get similar cases
+        relevant_responses = []
+        cases = []
+        for desc in scored_list:
+            if desc[1] < 47:
+                break
+            relevant_responses.append(desc[0])
+            doc = db.collection('emergency_history').where('description', '==', desc[0]).get()
+            doc = list(doc)[0].to_dict()
+            cases.append(doc)
+        self.write(json.dumps({"status": 200,
+                               "message":"successful",
+                               "relevant_responses": relevant_responses,
+                               "cases": cases}))
 
+    def write_error(self, status_code, message="Internal Server Error", **kwargs):
+        jsonData = {
+            'status': int(status_code),
+            'message': message
+        }
+        self.write(json.dumps(jsonData))
+
+    def options(self):
+        self.set_status(204)
+        self.finish()
 
